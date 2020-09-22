5795d5c75c1e1ac7ea1e298ac9528ca44452c426
Chris Roback <RobackChristopher@gmail.com>
2020-09-21 22:47:11 -0400

    working on inputs

--- client/components/Form.js
+++ client/components/Form.js
@@ -3,18 +3,35 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import regeneratorRuntime from "regenerator-runtime";
+import { HeatmapLayer } from '@react-google-maps/api';
 
 const SubmitForm = ({ formData }) => {
 
   const [fullNameInput, setFullNameInput] = useState("");
+  const [canSubmitName, setCanSubmitName] = useState(false);
   const [emailInput, setEmailInput] = useState("");
   const [numberInput, setNumberInput] = useState("");
   const [messageInput, setMessageInput] = useState("");
+
+  const nameOnChange = (e) => {
+    try {
+    setFullNameInput(e.target.value);
+    if (e.target.value.indexOf(' ') === -1) {
+      console.log('error bc you typed in', e.target.value);
+    } else {
+      setCanSubmitName(true);
+      
+    }
+  } catch (err) {
+    console.log('err=', err.message);
+  }
+  }
 
   const onSubmitForm = async ( e ) => {
     e.preventDefault();
 
     try {
+      if (canSubmitName){
       const body = { 
         "fullname": fullNameInput,
         "email": "howdy",
@@ -28,8 +45,10 @@ const SubmitForm = ({ formData }) => {
         body: JSON.stringify(body)
       });
       console.log('response', response);
+    }
     } catch (err) {
       console.error(err.message);
+      console.error('might not meet the name requirements');
     }
   }
 
@@ -43,7 +62,8 @@ const SubmitForm = ({ formData }) => {
         <Form.Control 
         type="text"
         value={fullNameInput} 
-        onChange={ e => setFullNameInput(e.target.value)}
+        // onChange={ e => setFullNameInput(e.target.value, console.log('value', e.target.value))}
+        onChange={(e) => nameOnChange(e)}
         type="usersName" 
         placeholder={formData[1]} />
         <Form.Text className="text-muted">{formData[11]}</Form.Text>
